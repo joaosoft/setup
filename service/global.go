5503d9bd2c34b216c6ea23f2ad332d90fcd8d6c5
João Ribeiro <joaosoft@gmail.com>
2018-04-11 11:13:11 +0100

    update log

--- service/global.go
+++ service/global.go
@@ -1,21 +1,11 @@
 package gosetup
 
 import (
-	"os"
-
 	logger "github.com/joaosoft/go-log/service"
-	writer "github.com/joaosoft/go-writer/service"
-
 )
 
 var global = make(map[string]interface{})
-var log = logger.NewLog(
-	logger.WithLevel(logger.InfoLevel),
-	logger.WithFormatHandler(writer.JsonFormatHandler),
-	logger.WithWriter(os.Stdout)).WithPrefixes(map[string]interface{}{
-	"level":   logger.LEVEL,
-	"time":    logger.TIME,
-	"service": "go-setup"})
+var log = logger.NewLogDefault("go-setup", logger.InfoLevel)
 
 func getDefaultNsqConfig() *NsqConfig {
 	if value, exists := global["nsq"]; exists {
